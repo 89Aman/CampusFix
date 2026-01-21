2cf0d583f5878c316687897e22989289b9c807d2
Aman Sharma <shasarita23@gmail.com>
2026-01-22 03:05:22 +0530

    Fix RouterLinkActive import error

--- Frontend/src/app/components/layout/layout.component.ts
+++ Frontend/src/app/components/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { switchMap, map, of } from 'rxjs';
@@ -7,7 +7,7 @@ import { switchMap, map, of } from 'rxjs';
 @Component({
     selector: 'app-layout',
     standalone: true,
-    imports: [RouterOutlet, RouterLink, CommonModule],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
     templateUrl: './layout.component.html',
     styleUrl: './layout.component.css'
 })
