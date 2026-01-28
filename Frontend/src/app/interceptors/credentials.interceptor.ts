4cfa85309e69a251300d791e9d36b15b441d0a30
Aman Sharma <shasarita23@gmail.com>
2026-01-28 15:00:21 +0530

    Delete Frontend-Angular

--- Frontend/src/app/interceptors/credentials.interceptor.ts
+++
@@ -1,8 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
-    const clonedReq = req.clone({
-        withCredentials: true
-    });
-    return next(clonedReq);
-};
